028b4f9800d937c179915c16dbef36f9119d8a8c
Piotr Pawelec <ppiotr352@gmail.com>
2026-02-28 17:17:38 +0100

    models

--- policyflux/core/complex_actor.py
+++ policyflux/core/complex_actor.py
@@ -19,3 +19,22 @@ class ComplexActor(ABC):
     def get_influence(self) -> float:
         """Return this actor's primary influence metric [0, 1]."""
         pass
+
+class CongressMember(ABC):
+    def __init__(self, id: int, yes_chance: float | None = None) -> None:
+        self.id: int = id
+        self.yes_chance: float = yes_chance if yes_chance is not None else 0.5
+
+    @abstractmethod
+    def vote(self, bill: Bill, **kwargs: Any) -> bool:
+        """
+        Docstring for vote
+
+        :param self: Description
+        :param bill: Description
+        :type bill: Bill
+        :param kwargs: Description
+        :return: Description
+        :rtype: bool
+        """
+        pass
